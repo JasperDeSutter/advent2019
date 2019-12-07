612840645c1dc274123fc23cc4f2c557a5866a88
JasperDeSutter <jasper.desutter@gmail.com>
2019-12-07 11:47:01 +0100

    rustfmt

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,126 +1,126 @@
 const MODULES_MASS: &[u32] = &[
-    106_404, 140_515, 142_745, 120_767, 79665, 54235, 127_391, 72207, 70799, 79485, 103_994,
-    129_583, 132_791, 95135, 121_194, 129_425, 64861, 123_233, 132_805, 87916, 111_395, 126_625,
-    113_045, 61704, 65413, 145_820, 75988, 74717, 115_137, 85331, 86833, 86063, 85464, 139_738,
-    103_372, 101_942, 52741, 77660, 112_745, 103_109, 106_301, 141_714, 74546, 55474, 106_747,
-    140_234, 60426, 145_867, 144_810, 94179, 101_606, 77763, 139_291, 104_246, 148_513, 126_828,
-    64624, 139_058, 85839, 86636, 62198, 137_358, 76711, 87848, 141_711, 114_079, 71639, 95896,
-    104_522, 61929, 72199, 142_790, 137_736, 123_437, 91872, 127_661, 111_179, 51548, 83452, 91196,
-    117_798, 84484, 75517, 83820, 97407, 89181, 71428, 72758, 73076, 109_957, 50601, 74571, 65556,
-    129_765, 80626, 126_995, 73480, 71360, 103_288, 85670,
+  106_404, 140_515, 142_745, 120_767, 79665, 54235, 127_391, 72207, 70799, 79485, 103_994, 129_583,
+  132_791, 95135, 121_194, 129_425, 64861, 123_233, 132_805, 87916, 111_395, 126_625, 113_045,
+  61704, 65413, 145_820, 75988, 74717, 115_137, 85331, 86833, 86063, 85464, 139_738, 103_372,
+  101_942, 52741, 77660, 112_745, 103_109, 106_301, 141_714, 74546, 55474, 106_747, 140_234, 60426,
+  145_867, 144_810, 94179, 101_606, 77763, 139_291, 104_246, 148_513, 126_828, 64624, 139_058,
+  85839, 86636, 62198, 137_358, 76711, 87848, 141_711, 114_079, 71639, 95896, 104_522, 61929,
+  72199, 142_790, 137_736, 123_437, 91872, 127_661, 111_179, 51548, 83452, 91196, 117_798, 84484,
+  75517, 83820, 97407, 89181, 71428, 72758, 73076, 109_957, 50601, 74571, 65556, 129_765, 80626,
+  126_995, 73480, 71360, 103_288, 85670,
 ];
 
 pub fn main() {
-    let total_fuel: u32 = MODULES_MASS.iter().copied().map(mass_required_fuel).sum();
-    println!("the total required fuel for all modules is: {}", total_fuel);
+  let total_fuel: u32 = MODULES_MASS.iter().copied().map(mass_required_fuel).sum();
+  println!("the total required fuel for all modules is: {}", total_fuel);
 
-    let total_fuel: u32 = MODULES_MASS.iter().copied().map(required_fuel).sum();
-    println!(
-        "the total required fuel for all modules including fuel is: {}",
-        total_fuel
-    );
+  let total_fuel: u32 = MODULES_MASS.iter().copied().map(required_fuel).sum();
+  println!(
+    "the total required fuel for all modules including fuel is: {}",
+    total_fuel
+  );
 }
 
 fn calculate_fuel(mass: u32) -> Option<u32> {
-    mass.checked_div(3)?.checked_sub(2)
+  mass.checked_div(3)?.checked_sub(2)
 }
 
 fn mass_required_fuel(mass: u32) -> u32 {
-    calculate_fuel(mass).unwrap_or_default()
+  calculate_fuel(mass).unwrap_or_default()
 }
 
 fn required_fuel(mass: u32) -> u32 {
-    std::iter::successors(Some(mass), |m| calculate_fuel(*m))
-        .skip(1)
-        .sum()
+  std::iter::successors(Some(mass), |m| calculate_fuel(*m))
+    .skip(1)
+    .sum()
 }
 
 #[cfg(test)]
 mod part1 {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_example_1() {
-        assert_eq!(
-            mass_required_fuel(12),
-            2,
-            "For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2."
-        )
-    }
+  #[test]
+  fn test_example_1() {
+    assert_eq!(
+      mass_required_fuel(12),
+      2,
+      "For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2."
+    )
+  }
 
-    #[test]
-    fn test_example_2() {
-        assert_eq!(
+  #[test]
+  fn test_example_2() {
+    assert_eq!(
             mass_required_fuel(14),
             2,
             "For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2."
         )
-    }
+  }
 
-    #[test]
-    fn test_example_3() {
-        assert_eq!(
-            mass_required_fuel(1969),
-            654,
-            "For a mass of 1969, the fuel required is 654."
-        )
-    }
+  #[test]
+  fn test_example_3() {
+    assert_eq!(
+      mass_required_fuel(1969),
+      654,
+      "For a mass of 1969, the fuel required is 654."
+    )
+  }
 
-    #[test]
-    fn test_example_4() {
-        assert_eq!(
-            mass_required_fuel(100_756),
-            33583,
-            "For a mass of 100756, the fuel required is 33583."
-        )
-    }
+  #[test]
+  fn test_example_4() {
+    assert_eq!(
+      mass_required_fuel(100_756),
+      33583,
+      "For a mass of 100756, the fuel required is 33583."
+    )
+  }
 
-    #[test]
-    fn test_zero() {
-        assert_eq!(mass_required_fuel(0), 0)
-    }
+  #[test]
+  fn test_zero() {
+    assert_eq!(mass_required_fuel(0), 0)
+  }
 }
 
 #[cfg(test)]
 mod part2 {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_example_1() {
-        assert_eq!(
-            required_fuel(12),
-            2,
-            "A module of mass 14 requires 2 fuel. This fuel requires no further
+  #[test]
+  fn test_example_1() {
+    assert_eq!(
+      required_fuel(12),
+      2,
+      "A module of mass 14 requires 2 fuel. This fuel requires no further
             fuel (2 divided by 3 and rounded down is 0, which would call for a
             negative fuel), so the total fuel required is still just 2."
-        )
-    }
+    )
+  }
 
-    #[test]
-    fn test_example_2() {
-        assert_eq!(
-            required_fuel(1969),
-            966,
-            "At first, a module of mass 1969 requires 654 fuel. Then, this fuel
+  #[test]
+  fn test_example_2() {
+    assert_eq!(
+      required_fuel(1969),
+      966,
+      "At first, a module of mass 1969 requires 654 fuel. Then, this fuel
             requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel,
             which requires 21 fuel, which requires 5 fuel, which requires no
             further fuel. So, the total fuel required for a module of mass 1969 is
             654 + 216 + 70 + 21 + 5 = 966"
-        )
-    }
+    )
+  }
 
-    #[test]
-    fn test_example_3() {
-        assert_eq!(
-            required_fuel(100_756),
-            50346,
-            "The fuel required by a module of mass 100756 and its fuel is:
+  #[test]
+  fn test_example_3() {
+    assert_eq!(
+      required_fuel(100_756),
+      50346,
+      "The fuel required by a module of mass 100756 and its fuel is:
             33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346."
-        )
-    }
+    )
+  }
 
-    #[test]
-    fn test_zero() {
-        assert_eq!(required_fuel(0), 0)
-    }
+  #[test]
+  fn test_zero() {
+    assert_eq!(required_fuel(0), 0)
+  }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,146 +1,167 @@
 pub struct Program {
-    code: Vec<i32>,
-    ptr: usize,
-    pub input: Option<i32>,
-    pub output: Option<i32>,
+  code: Vec<i32>,
+  ptr: usize,
+  pub input: Option<i32>,
+  pub output: Option<i32>,
 }
 
 impl Program {
-    pub fn new(code: impl Into<Vec<i32>>) -> Self {
-        Self {
-            code: code.into(),
-            ptr: 0,
-            input: None,
-            output: None,
+  pub fn new(code: impl Into<Vec<i32>>) -> Self {
+    Self {
+      code: code.into(),
+      ptr: 0,
+      input: None,
+      output: None,
+    }
+  }
+
+  fn read(&self, offset: usize) -> i32 {
+    self.code[self.ptr + offset]
+  }
+
+  fn arg_value(&self, mode: &[ParamMode], idx: usize) -> i32 {
+    match mode[idx] {
+      ParamMode::Immediate => self.read(idx + 1),
+      ParamMode::Position => self.code[self.read(idx + 1) as usize],
+    }
+  }
+
+  pub fn run(&mut self) {
+    assert!(self.ptr == 0, "can't run programs twice!");
+    loop {
+      let opcode = Opcode::from(self.read(0));
+      match opcode {
+        Opcode::Add(mode) => {
+          let value = self.arg_value(&mode, 0) + self.arg_value(&mode, 1);
+          let pos = self.read(3) as usize;
+          self.code[pos] = value;
+          self.ptr += 3 + 1;
         }
-    }
-
-    fn read(&self, offset: usize) -> i32 {
-        self.code[self.ptr + offset]
-    }
-
-    fn arg_value(&self, mode: &[ParamMode], idx: usize) -> i32 {
-        match mode[idx] {
-            ParamMode::Immediate => self.read(idx + 1),
-            ParamMode::Position => self.code[self.read(idx + 1) as usize],
+        Opcode::Mul(mode) => {
+          let value = self.arg_value(&mode, 0) * self.arg_value(&mode, 1);
+          let pos = self.read(3) as usize;
+          self.code[pos] = value;
+          self.ptr += 3 + 1;
         }
-    }
-
-    pub fn run(&mut self) {
-        assert!(self.ptr == 0, "can't run programs twice!");
-        loop {
-            let opcode = Opcode::from(self.read(0));
-            match opcode {
-                Opcode::Add(mode) => {
-                    let value = self.arg_value(&mode, 0) + self.arg_value(&mode, 1);
-                    let pos = self.read(3) as usize;
-                    self.code[pos] = value;
-                    self.ptr += 3 + 1;
-                }
-                Opcode::Mul(mode) => {
-                    let value = self.arg_value(&mode, 0) * self.arg_value(&mode, 1);
-                    let pos = self.read(3) as usize;
-                    self.code[pos] = value;
-                    self.ptr += 3 + 1;
-                }
-                Opcode::Input => {
-                    let pos = self.read(1) as usize;
-                    self.code[pos] = self.input.unwrap();
-                    self.ptr += 1 + 1;
-                }
-                Opcode::Output(mode) => {
-                    self.output = Some(self.arg_value(&mode, 0));
-                    self.ptr += 1 + 1;
-                }
-                Opcode::JumpIfTrue(mode) => {
-                    self.ptr = match self.arg_value(&mode, 0) {
-                        0 => self.ptr + 2 + 1,
-                        _ => self.arg_value(&mode, 1) as usize,
-                    }
-                }
-                Opcode::JumpIfFalse(mode) => {
-                    self.ptr = match self.arg_value(&mode, 0) {
-                        0 => self.arg_value(&mode, 1) as usize,
-                        _ => self.ptr + 2 + 1,
-                    }
-                }
-                Opcode::LessThan(mode) => {
-                    let value = self.arg_value(&mode, 0) < self.arg_value(&mode, 1);
-                    let pos = self.read(3) as usize;
-                    self.code[pos] = value as i32;
-                    self.ptr += 3 + 1;
-                }
-                Opcode::Equals(mode) => {
-                    let value = self.arg_value(&mode, 0) == self.arg_value(&mode, 1);
-                    let pos = self.read(3) as usize;
-                    self.code[pos] = value as i32;
-                    self.ptr += 3 + 1;
-                }
-                Opcode::Exit => break,
-            }
+        Opcode::Input => {
+          let pos = self.read(1) as usize;
+          println!("getting input");
+          self.code[pos] = self.input.unwrap();
+          self.ptr += 1 + 1;
         }
+        Opcode::Output(mode) => {
+          self.output = Some(self.arg_value(&mode, 0));
+          self.ptr += 1 + 1;
+        }
+        Opcode::JumpIfTrue(mode) => {
+          self.ptr = match self.arg_value(&mode, 0) {
+            0 => self.ptr + 2 + 1,
+            _ => self.arg_value(&mode, 1) as usize,
+          }
+        }
+        Opcode::JumpIfFalse(mode) => {
+          self.ptr = match self.arg_value(&mode, 0) {
+            0 => self.arg_value(&mode, 1) as usize,
+            _ => self.ptr + 2 + 1,
+          }
+        }
+        Opcode::LessThan(mode) => {
+          let value = self.arg_value(&mode, 0) < self.arg_value(&mode, 1);
+          let pos = self.read(3) as usize;
+          self.code[pos] = value as i32;
+          self.ptr += 3 + 1;
+        }
+        Opcode::Equals(mode) => {
+          let value = self.arg_value(&mode, 0) == self.arg_value(&mode, 1);
+          let pos = self.read(3) as usize;
+          self.code[pos] = value as i32;
+          self.ptr += 3 + 1;
+        }
+        Opcode::Exit => break,
+      }
     }
+  }
 
-    pub fn code(&self) -> &[i32] {
-        &self.code
-    }
+  pub fn code(&self) -> &[i32] {
+    &self.code
+  }
 
-    pub fn into_code(self) -> Vec<i32> {
-        self.code
-    }
+  pub fn into_code(self) -> Vec<i32> {
+    self.code
+  }
 }
 
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
 enum Opcode {
-    Add([ParamMode; 2]),
-    Mul([ParamMode; 2]),
-    Input,
-    Output([ParamMode; 1]),
-    JumpIfTrue([ParamMode; 2]),
-    JumpIfFalse([ParamMode; 2]),
-    LessThan([ParamMode; 2]),
-    Equals([ParamMode; 2]),
-    Exit,
+  Add([ParamMode; 2]),
+  Mul([ParamMode; 2]),
+  Input,
+  Output([ParamMode; 1]),
+  JumpIfTrue([ParamMode; 2]),
+  JumpIfFalse([ParamMode; 2]),
+  LessThan([ParamMode; 2]),
+  Equals([ParamMode; 2]),
+  Exit,
 }
 
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
 enum ParamMode {
-    Position,
-    Immediate,
+  Position,
+  Immediate,
 }
 
 fn arg_mode_immediate(instruction: i32, arg: u32) -> ParamMode {
-    let decimal_pos = 10i32.pow(arg + 2);
-    match (instruction / decimal_pos) & 1 {
-        0 => ParamMode::Position,
-        _ => ParamMode::Immediate,
-    }
+  let decimal_pos = 10i32.pow(arg + 2);
+  match (instruction / decimal_pos) & 1 {
+    0 => ParamMode::Position,
+    _ => ParamMode::Immediate,
+  }
 }
 
 fn arg_mode_1(instruction: i32) -> [ParamMode; 1] {
-    [arg_mode_immediate(instruction, 0)]
+  [arg_mode_immediate(instruction, 0)]
 }
 
 fn arg_mode_2(instruction: i32) -> [ParamMode; 2] {
-    [
-        arg_mode_immediate(instruction, 0),
-        arg_mode_immediate(instruction, 1),
-    ]
+  [
+    arg_mode_immediate(instruction, 0),
+    arg_mode_immediate(instruction, 1),
+  ]
 }
 
 impl From<i32> for Opcode {
-    fn from(instruction: i32) -> Self {
-        match instruction % 100 {
-            1 => Opcode::Add(arg_mode_2(instruction)),
-            2 => Opcode::Mul(arg_mode_2(instruction)),
-            3 => Opcode::Input,
-            4 => Opcode::Output(arg_mode_1(instruction)),
-            5 => Opcode::JumpIfTrue(arg_mode_2(instruction)),
-            6 => Opcode::JumpIfFalse(arg_mode_2(instruction)),
-            7 => Opcode::LessThan(arg_mode_2(instruction)),
-            8 => Opcode::Equals(arg_mode_2(instruction)),
-            99 => Opcode::Exit,
-            _ => panic!("wrong opcode!"),
-        }
+  fn from(instruction: i32) -> Self {
+    match instruction % 100 {
+      1 => Opcode::Add(arg_mode_2(instruction)),
+      2 => Opcode::Mul(arg_mode_2(instruction)),
+      3 => Opcode::Input,
+      4 => Opcode::Output(arg_mode_1(instruction)),
+      5 => Opcode::JumpIfTrue(arg_mode_2(instruction)),
+      6 => Opcode::JumpIfFalse(arg_mode_2(instruction)),
+      7 => Opcode::LessThan(arg_mode_2(instruction)),
+      8 => Opcode::Equals(arg_mode_2(instruction)),
+      99 => Opcode::Exit,
+      _ => panic!("wrong opcode!"),
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn fibonacci() {
+    // https://github.com/emmericp/aoc19-intcode-jit/blob/master/fibonacci.intcode
+    const CODE: &[i32] = &[
+      1101, 0, 0, 0, 1101, 0, 0, 0, 3, 0, 1007, 0, 3, 5, 1006, 5, 20, 104, 1, 99, 1101, 0, 1, 1,
+      1101, 0, 1, 2, 1101, 0, 1, 3, 1101, 0, 2, 4, 8, 4, 0, 5, 1006, 5, 46, 4, 3, 99, 1, 1, 2, 3,
+      1001, 2, 0, 1, 1001, 3, 0, 2, 1001, 4, 1, 4, 1106, 0, 36,
+    ];
+
+    let mut program = Program::new(CODE);
+    program.input = Some(1000);
+    program.run();
+    println!("{:?}", program.output);
+  }
 }
